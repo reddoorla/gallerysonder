a76737da2aea1ab6bab0da6cf5dddac6c8c61e96
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-05-08 14:21:43 -0700

    page type changes

--- src/routes/[[preview=preview]]/+page.server.js
+++ src/routes/[[preview=preview]]/+page.server.js
@@ -6,10 +6,14 @@ export async function load({ fetch, cookies }) {
 	const client = createClient({ fetch, cookies });
 
 	const page = await client.getByUID('page', 'home');
+	const intro = await client.getSingle('intro_images')
+	const nav = await client.getSingle('nav')
 
 	return {
 		page,
-		title: asText(page.data.title),
+		intro,
+		nav,
+		title: "Sonder",
 		meta_description: page.data.meta_description,
 		meta_title: page.data.meta_title,
 		meta_image: page.data.meta_image.url
