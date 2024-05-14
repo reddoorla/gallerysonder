1bc2a45fe7ab0e7303c1930c0641a9434edc89ef
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2024-05-14 11:50:43 -0700

    mix blend mode

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -21,14 +21,14 @@
 
 </script>
 
-<div class=" w-screen fixed h-24 top-0 py-8 z-50 pointer-events-none" transition:fade={{ duration: 300 }}>
+<div class=" w-screen fixed h-24 top-0 py-8 z-50 pointer-events-none mix-blend-difference " transition:fade={{ duration: 300 }}>
     <ContentWidth class="flex flex-row justify-between items-center">
-        <button class="scale-105 text-white hover:text-accent-pink pointer-events-auto filter-to-accent-pink-on-hover active:invert transition-all {isLogoBlack||showNav ? "brightness-0" : ""}" on:click={()=>showNav = !showNav}>
+        <button class="scale-105 text-white hover:text-accent-pink hover:mix-blend-normal pointer-events-auto filter-to-accent-pink-on-hover active:invert transition-all {isLogoBlack||showNav ? "brightness-0" : ""}" on:click={()=>showNav = !showNav}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M20 18C20.5523 18 21 17.5523 21 17C21 16.4477 20.5523 16 20 16H4C3.44772 16 3 16.4477 3 17C3 17.5523 3.44772 18 4 18H20ZM20 13C20.5523 13 21 12.5523 21 12C21 11.4477 20.5523 11 20 11H4C3.44772 11 3 11.4477 3 12C3 12.5523 3.44772 13 4 13H20ZM3 7C3 7.55228 3.44772 8 4 8H20C20.5523 8 21 7.55228 21 7C21 6.44772 20.5523 6 20 6H4C3.44772 6 3 6.44772 3 7Z" fill="white"/>                        
             </svg>
         </button>
-        <a href="/" class="filter-to-accent-pink-on-hover transition-colors pointer-events-auto bump brightness-0 {isLogoBlack||showNav ? "" : "invert"}">
+        <a href="/" class="filter-to-accent-pink-on-hover transition-colors pointer-events-auto bump brightness-0 hover:mix-blend-normal {isLogoBlack||showNav ? "" : "invert"}">
             <RotatingLogo class="h-6" />
         </a>
     </ContentWidth>
